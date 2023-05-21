800f2402221d40796f2da0ccc2069e9154e11d3a
Shubham0850 <rajshubham0850@gmail.com>
2023-05-21 16:57:59 +0530

    main rust file

---
+++ Codes/src/main.rs
@@ -0,0 +1,12 @@
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+
+fn main() {
+    let stdout = stdout();
+    let message = String::from("Hello fellow Rustaceans!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(message.as_str(), width, &mut writer).unwrap();
+}
+    
