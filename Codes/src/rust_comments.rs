c964da540e2633e856b63fe74e8849cc4874a929
Shubham0850 <rajshubham0850@gmail.com>
2023-05-21 16:58:58 +0530

    comments

---
+++ Codes/src/rust_comments.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // This is an example of a line comment.
+
+    /*
+    	* This is another type of comment, a block comment.
+    	* We can also say that, it's a multiline comment
+     */
+
+    println!("Comments in rust");
+}
